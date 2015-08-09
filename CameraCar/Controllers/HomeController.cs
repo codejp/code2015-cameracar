bd0cec0b89b9c03d7f7b448f7652ae71c9fcb543
jsakamoto <junichi.jp@gmail.com>
2015-08-09 10:53:11 +0900

    Implement gallery feature.

--- CameraCar/Controllers/HomeController.cs
+++ CameraCar/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,6 +13,27 @@ namespace CameraCar.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+
+        public ActionResult gallery()
+        {
+            var saveDir = Server.MapPath("~/App_Data");
+            var files = System.IO.Directory
+                .GetFiles(saveDir, "photo*.jpg")
+                .OrderBy(name => name);
+
+            return View(files);
+        }
+
+        public ActionResult photo(string id)
+        {
+            if (Regex.IsMatch(id ?? "", @"^photo\d{3}$") == false)
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            var saveDir = Server.MapPath("~/App_Data");
+            var imagPath = System.IO.Path.Combine(saveDir, id + ".jpg");
+
+            return File(imagPath, "image/jpeg");
         }
     }
 }
